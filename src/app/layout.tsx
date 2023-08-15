750d1a7d0136b7210df747bfbd4cde1ea9d84a2b
nebular-lab <hiranodaisuke0721@gmail.com>
2023-08-15 17:31:52 +0900

    faviconwo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import { Providers } from './providers';
-
 
 export const metadata: Metadata = {
   title: 'History Image Generator',
@@ -15,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-  <html lang="ja">
-      <body className='flex justify-center'>
+    <html lang="ja">
+      <body className="flex justify-center">
         <Providers>{children}</Providers>
       </body>
     </html>
