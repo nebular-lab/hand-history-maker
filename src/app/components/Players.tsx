e479130059e6aecec8e0d4c89a51fc9b9b288788
nebular-lab <hiranodaisuke0721@gmail.com>
2023-08-15 17:48:45 +0900

    レイアウト調整

--- src/app/components/Players.tsx
+++ src/app/components/Players.tsx
@@ -12,13 +12,11 @@ export const Players: FC<Props> = (props) => {
   const { players, XBP, ES } = props;
   return (
     <div className="flex items-center h-[65px] gap-2">
-      <div className=" flex gap-2 justify-center items-center ">
-        <div className="text-white bg-position w-fit p-2 font-sans font-medium rounded-xl border-2 border-action-border">
-          {ES == '' ? 'x' : ES}BB
-        </div>
-        <div className="text-white bg-position w-fit p-2 font-sans font-medium rounded-xl border-2 border-action-border">
-          {XBP == '' ? 'x' : XBP}BP
-        </div>
+      <div className="text-white flex items-center px-3 h-full bg-position w-fit  font-sans font-medium rounded-xl border-2 border-action-border text-xl">
+        {ES == '' ? 'x' : ES}BB
+      </div>
+      <div className="text-white flex items-center px-3 h-full bg-position w-fit  font-sans font-medium rounded-xl border-2 border-action-border text-xl">
+        {XBP == '' ? 'x' : XBP}BP
       </div>
       <div className="flex gap-2">
         {players.map((player) => (
